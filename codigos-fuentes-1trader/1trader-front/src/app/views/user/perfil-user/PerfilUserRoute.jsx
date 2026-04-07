cbbaf59716926851f26029d5115a977eb807db85
Victor Hugo Ibarguen Urrutia <victor.ibarguen@ghtcorp.com>
2026-04-06 19:57:48 -0500

    R2

--- codigos-fuentes-1trader/1trader-front/src/app/views/user/perfil-user/PerfilUserRoute.jsx
+++
@@ -1,8 +0,0 @@
-import Loadable from 'app/components/Loadable';
-import { lazy } from 'react';
-
-const PerfilUser = Loadable(lazy(async () => import('./PerfilUser')));
-
-const perfilUserRoute = [{ path: '/customer/perfil', element: <PerfilUser /> }];
-
-export default perfilUserRoute;
